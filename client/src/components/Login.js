f09f8cbd70c14f1479dd0aa1838d1608182f5c89
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-23 16:40:27 +0100

    WIP: now redirecting to <BubblePage />  upon login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios'
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const [loginDetails, setLoginDetails] = useState({
@@ -25,6 +25,7 @@ const Login = () => {
       .then(result => {
         console.log('✅ axios login result: ', result.data.payload)
         localStorage.setItem('token', result.data.payload)
+        props.history.push("/bubblespage");
       })
       .catch(error => console.log('❌ axios login error: ', error))
   }
